8722afa5965d61f71ed100b4d00e2253a3655576
Lucas Cima <lucas.cima92@gmail.com>
2018-07-26 12:07:01 -0500

    FSDF

---
+++ src/main/java/com/autoworld/autoworld/AutoWorldApplication.java
@@ -0,0 +1,12 @@
+package com.autoworld.autoworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoWorldApplication.class, args);
+	}
+}
---
+++ src/test/java/com/autoworld/autoworld/AutoWorldApplicationTests.java
@@ -0,0 +1,16 @@
+package com.autoworld.autoworld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutoWorldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
